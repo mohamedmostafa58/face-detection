8631fc03dca8f9ef045d7193a4bcabcaec7c8bd5
mohamedmostafa58 <mohamedmostafa58113@gmail.com>
2023-09-10 11:26:13 +0300

    canvas

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -5,6 +5,7 @@ import * as faceapi from "face-api.js";
 function App() {
   const videoref = useRef(null);
   const frameref = useRef(null);
+  const canvasRef = useRef(null);
   const [isFaceDetected, setIsFaceDetected] = useState(false);
   const [isFaceTooClose, setIsFaceTooClose] = useState(false);
 
@@ -29,21 +30,34 @@ function App() {
     const interval = setInterval(async () => {
       const video = videoref.current;
       const frame = frameref.current;
-      if (video && frame) {
-        // Check if the video is playing
+      const canvas = canvasRef.current;
+      if (video && frame && canvas) {
         if (video.videoWidth > 0 && video.videoHeight > 0) {
           const frameRect = frame.getBoundingClientRect();
           const videoRect = video.getBoundingClientRect();
           const detections = await faceapi
             .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
             .withFaceLandmarks();
-
+          const ctx = canvas.getContext("2d");
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
           const resizedDetections = faceapi.resizeResults(detections, {
             width: video.videoWidth,
             height: video.videoHeight,
           });
 
           if (resizedDetections.length > 0) {
+            const faceInRoi = resizedDetections[0].detection.box;
+            console.log("Face Detection Coordinates:", faceInRoi);
+            ctx.strokeStyle = "red";
+            ctx.lineWidth = 2;
+            ctx.beginPath();
+            ctx.rect(
+              faceInRoi.x,
+              faceInRoi.y,
+              faceInRoi.width,
+              faceInRoi.height
+            );
+            ctx.stroke();
             const roi = {
               x: frameRect.left - videoRect.left,
               y: frameRect.top - videoRect.top,
@@ -51,7 +65,7 @@ function App() {
               height: frameRect.height,
             };
             setIsFaceDetected(true);
-            const faceInRoi = resizedDetections[0].detection.box;
+
             if (
               faceInRoi.x >= roi.x &&
               faceInRoi.y >= roi.y &&
@@ -81,6 +95,12 @@ function App() {
         className={styles.video}
       ></video>
       <div className={styles.frame} ref={frameref}></div>
+      <canvas
+        ref={canvasRef}
+        className={styles.canvas}
+        width="800"
+        height="800"
+      ></canvas>
       {isFaceDetected ? (
         <h3>
           {isFaceTooClose ? "verified" : "put your face in the gray frame"}
